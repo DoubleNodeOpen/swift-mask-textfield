6af64df1e0f57525dee4beac4f079b3b6a7bf2c0
Darren Ehlers <me@darrenehlers.com>
2022-06-26 12:11:54 -0500

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.6
+//
+//  Package.swift
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMaskTextfield",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftMaskTextfield",
+            type: .static,
+            targets: ["SwiftMaskTextfield"]),
+    ],
+    dependencies: [],
+    targets: [
+         .target(
+             name: "SwiftMaskTextfield",
+             dependencies: []
+         )
+     ]
+)
